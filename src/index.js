71061c348110fb414f21d14dd232aa4bd81d2d54
Alheli De Vicente <devicentealheli@gmail.com>
2024-05-24 17:01:03 -0600

    corrected html, finished index.js and updated css according to html changes

--- src/index.js
+++ src/index.js
@@ -1,27 +1,23 @@
 import analyzer from './analyzer.js';
-const metricsFunctions = [
-analyzer.getWordCount,
-analyzer.getCharacterCount,
-analyzer.getCharacterCountExcludingSpaces,
-analyzer.getAverageWordLength,
-analyzer.getNumberCount,
-analyzer.getNumberSum
-]
-const metricUpdates = [
-  document.querySelector("total-words"),
-  document.querySelector(".totalchar"),
-  document.querySelector ("chars-without-spaces"),
-  document.querySelector("average-words"),
-  document.querySelector("num"),
-  document.querySelector("sum-nums"),
-]
-const resetButton = document.getElementById("reset-button")
+const wordCount = document.querySelector('[data-testid="word-count"]')
+const totalChar = document.querySelector('[data-testid="character-count"]')
+const totalCharNoSpaces = document.querySelector('[data-testid="character-no-spaces-count"]')
+const average = document.querySelector('[data-testid="word-length-average"]')
+const num = document.querySelector('[data-testid="number-count"]')
+const sumNum = document.querySelector('[data-testid ="number-sum"')
 const textArea = document.querySelector('textarea[name="user-input"]')
+const button = document.getElementById("reset-button")
+textArea.addEventListener("input", results)
+function results (){
+  wordCount.textContent = analyzer.getWordCount(textArea.value)
+  totalChar.textContent = analyzer.getCharacterCount(textArea.value)
+  totalCharNoSpaces.textContent = analyzer.getCharacterCountExcludingSpaces(textArea.value)
+  average.textContent = analyzer.getAverageWordLength(textArea.value)
+  num.textContent = analyzer.getNumberCount(textArea.value)
+  sumNum.textContent = analyzer.getNumberSum(textArea.value)
+}
+button.addEventListener("click", clean)
 function clean (){
   textArea.value = ""
+  results()
 }
-resetButton.addEventListener("click", clean);
-function updateValue() {
-  metricUpdates.forEach(element, index, array)
-}
-textArea.addEventListener("input", updateValue);
